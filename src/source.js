988d000196871e45cdb41218df1b1899e8ff9498
searchme86 <searchme86@gmail.com>
2021-11-27 00:26:03 +0900

    script for input validate checked, 20211127

--- src/source.js
+++ src/source.js
@@ -1,17 +1,14 @@
 (function () {
-	const boxes = Array.from(document.querySelectorAll('.form-info'))
+	
 	const inputs = Array.from(document.querySelectorAll('.form-input input'))
 	const labels = Array.from(document.querySelectorAll('.form-input label'))
-
-	const states = ['valid', 'invalid'];
-	let classes = '';	
+	const Signbtn = document.querySelector('.form-actions .btn-signIn')
 
 	const validateform = (e) => {
 		
 		inputs.forEach((input)=> input.classList.remove())
 		labels.forEach((label)=> label.classList.remove())
-		boxes.forEach((box)=> box.classList.remove())
-
+		
 		for(let i=0; i<labels.length; i++){
 			labels[i].addEventListener('click',function(e){
 				e.target.parentNode.parentNode.classList.add('on');
@@ -29,18 +26,16 @@
 			})
 		}
 
-		for(let i=0; i<inputs.length; i++){
-			inputs[i].addEventListener('blur',function(e){
-				e.target.parentNode.parentNode.classList.remove('on');
-				e.stopPropagation();
-			})
-		}
-
-		for(let i=0; i<inputs.length; i++){
-			inputs[i].addEventListener('input',function(e){
-				if(!!e.target.value.length){
+		for (let i = 0; i < inputs.length; i++) {
+			inputs[i].addEventListener('input', function (e) {
+				if (!!e.target.value.length) {
 					e.target.parentNode.parentNode.classList.add('on')
-				}else{
+					if (e.target.parentNode.parentNode.querySelector('.alert') !== null) {
+							e.target.parentNode.parentNode.classList.remove('inValid');
+							console.log(e.target.parentNode.nextElementSibling)
+							e.target.parentNode.nextElementSibling.remove()
+						}
+				} else {
 					e.target.parentNode.parentNode.classList.remove('on')
 				}
 				e.stopPropagation();
@@ -48,30 +43,50 @@
 		}
 
 		for(let i=0; i<inputs.length; i++){
-			inputs[i].addEventListener('blur',function(e){
+			inputs[i].addEventListener('blur', function (e) {
+				e.target.parentNode.parentNode.classList.remove('on');
 				if(!!e.target.value.length){
 					e.target.previousElementSibling.style.opacity=0;
 					e.target.previousElementSibling.style.transition='all .5s';
-					e.stopPropagation();	
 				}
+				e.stopPropagation();	
 			})
 		}
 
-		for(let i=0; i<inputs.length; i++){
-			inputs[i].addEventListener('click',function(e){
-				e.target.parentNode.parentNode.classList.add('on');
-				// let currentIdx = i;
-				// let newArray = [];
-				// let anotherItem;
-				// anotherItem = inputs.splice(currentIdx,1)
-				// newArray.push(anotherItem);
-				// console.log(inputs)
-				// console.log(currentIdx)
-				// console.log(newArray)
-				e.stopPropagation();
-			})
-		}
+
+		Signbtn.addEventListener('click', function (e) {
+			const boxes = Array.from(document.querySelectorAll('.form-info'))
+			boxes.forEach((box) => box.classList.remove())
+			const warning= document.querySelectorAll('.alert')
+			const list = [];
+
+			if (!warning.length) {
+				const errorDiv = (i) => {
+					const errorDiv = document.createElement('div');
+					errorDiv.appendChild(document.createTextNode('입력값을 넣어주세요'))
+					errorDiv.classList.add('alert')
+					boxes[i].insertAdjacentElement('beforeEnd',errorDiv)
+				}
+				for (let i=0; i < inputs.length; i++){
+					if (inputs[i].className='required' && !inputs[i].value) {
+						inputs[i].parentNode.parentNode.classList.add('inValid');
+						errorDiv(i)
+					}else{
+						console.log('모두 값이 있다')
+						list.push(i)
+					}
+				}
+			}
+			if (list.length === inputs.length) {
+				alert('축하합니다. 다음 페이지로 이동하세요')
+			}
+			e.stopPropagation();
+		})
+
+
+
 	}
 
 	validateform();
+
 }())
